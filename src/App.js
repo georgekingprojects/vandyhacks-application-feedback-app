744f038e5428ba19a60a8a14698021e1c3eec846
George King <george.y.wang@vanderbilt.edu>
2023-01-27 13:37:09 -0600

    added states and inputs for message and star ratings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Rating } from "react-simple-star-rating";
 
 function App() {
+  const [message, setMessage] = React.useState("");
+  const [rating, setRating] = React.useState(0);
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+  const handleRating = (rate: number) => {
+    setRating(rate);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="app__form" onSubmit={handleSubmit}>
+        <label>Send your feedback to VandyHacks!</label>
+        <input
+          type="text"
+          placeholder="Your comments here."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <Rating onClick={handleRating} />
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>{rating}</p>
     </div>
   );
 }
